eb0dcfb253697641e938bd44ffc23fd540605b1b
tutuzjy <1810265321@qq.com>
2023-12-04 20:46:44 +0800

    finish KVBlock

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
+#![allow(unused)]
+mod cfg;
 mod numa;
 mod race;
-mod cfg;
 
-use race::race_type::Directories;
 use numa::mm::MemoryManager;
-use std::sync::{Arc, Mutex};
-
+use race::race_type::{Directories, KVBlockMem};
+use std::{
+    ops::Deref,
+    sync::{Arc, Mutex},
+};
 fn main() {
     let memory_manager = Arc::new(Mutex::new(MemoryManager::new()));
-    let mut directories = Directories::new();
+    let key = String::from("sentence");
+    let value = String::from("Hello World!");
+    let kvbm = std::mem::ManuallyDrop::new(KVBlockMem::new(
+        key.len() as u16,
+        value.len() as u16,
+        key,
+        value,
+        memory_manager,
+    ));
+    let kvb = unsafe { (**(kvbm.deref())).get() };
+    println!("{:?}", kvb);
+    // let mut directories = Directories::new();
 
-    directories.add_directory(memory_manager.clone());
+    // directories.add_directory(memory_manager.clone());
 
-    directories.add_directory(memory_manager.clone());
+    // directories.add_directory(memory_manager.clone());
 
-    // directories.deref_directories();
+    // // directories.deref_directories();
 
-    directories.get(0).get_subtable().bucket_groups[0].buckets[0].slots[0].set_length(10);
+    // directories.get(0).get_subtable().bucket_groups[0].buckets[0].slots[0].set_length(10);
 
-    print!("{}", directories.get(0).get_subtable().bucket_groups[0].buckets[0].slots[0].get_length());
+    // print!(
+    //     "{}",
+    //     directories.get(0).get_subtable().bucket_groups[0].buckets[0].slots[0].get_length()
+    // );
 
-    // print!("{}", directories.get(0).get_subtable().bucket_groups[0].buckets[0].header.data);
+    // // print!("{}", directories.get(0).get_subtable().bucket_groups[0].buckets[0].header.data);
 }
--- src/race/race_type.rs
+++ src/race/race_type.rs
@@ -1,5 +1,6 @@
 use super::super::cfg::config::CONFIG;
 use super::super::numa::mm::MemoryManager;
+use crc::{Crc, CRC_64_REDIS};
 use std::mem::size_of;
 use std::sync::{Arc, Mutex};
 
@@ -224,5 +225,79 @@ impl Directories {
 
     pub fn get(&mut self, index: usize) -> &mut Directory {
         unsafe { &mut *(self.sub_dirs[index]) }
+    }
+}
+#[derive(Debug)]
+pub struct KVBlock {
+    pub klen: u16,
+    pub vlen: u16,
+    pub key: String,
+    pub value: String,
+    pub crc64: u64,
+}
+
+pub struct KVBlockMem {
+    klen: u16,
+    vlen: u16,
+    crc64: u64,
+}
+
+impl KVBlockMem {
+    pub fn new(
+        kl: u16,
+        vl: u16,
+        key: String,
+        value: String,
+        memory_manager: Arc<Mutex<MemoryManager>>,
+    ) -> *const Self {
+        let kvblock_pointer = memory_manager
+            .lock()
+            .unwrap()
+            .malloc(size_of::<KVBlockMem>() + key.len() + value.len());
+        if kvblock_pointer == std::ptr::null_mut() {
+            panic!("kvblock malloc failed");
+        }
+        unsafe {
+            (*(kvblock_pointer as *mut Self)).klen = key.len() as u16;
+            (*(kvblock_pointer as *mut Self)).vlen = value.len() as u16;
+        }
+        let combined_string = key + &value;
+        let checksum = Crc::<u64>::new(&CRC_64_REDIS).checksum(combined_string.as_bytes());
+        unsafe {
+            (*(kvblock_pointer as *mut Self)).crc64 = checksum;
+        }
+        let kv_pointer = kvblock_pointer.wrapping_add(size_of::<KVBlockMem>());
+        let combined_iter = combined_string.as_bytes().iter();
+        unsafe {
+            let mut i = 0;
+            for chr in combined_iter {
+                *(kv_pointer.wrapping_add(i)) = *chr;
+                i += 1;
+            }
+        }
+        kvblock_pointer as *const Self
+    }
+
+    pub fn get(&self) -> KVBlock {
+        let kl = self.klen;
+        let vl = self.vlen;
+        let checksum = self.crc64;
+        let data_pointer = unsafe { std::mem::transmute::<&KVBlockMem, *mut u8>(self) }
+            .wrapping_add(size_of::<KVBlockMem>());
+        let key = unsafe { String::from_raw_parts(data_pointer, kl as usize, kl as usize) };
+        let value = unsafe {
+            String::from_raw_parts(
+                data_pointer.wrapping_add(kl as usize),
+                vl as usize,
+                vl as usize,
+            )
+        };
+        KVBlock {
+            klen: kl,
+            vlen: vl,
+            key,
+            value,
+            crc64: checksum,
+        }
     }
 }
